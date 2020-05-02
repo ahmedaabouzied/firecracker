f1c767ce5f99c3e85beca682159b20d05a21c063
Ahmed Abouzied <ahmedaabouzied44@gmail.com>
2020-05-02 05:59:40 +0200

    fix ... to ..
    Signed-off-by: Ahmed Abouzied <ahmedaabouzied44@gmail.com>

--- src/utils/src/net/tap.rs
+++ src/utils/src/net/tap.rs
@@ -646,7 +646,7 @@ mod tests {
     fn test_send_plain_data(){
         let tap1 = Tap::open_named("plaindata").unwrap();
         let data = "plain data".as_bytes();
-        tap1.write(&data[pos...]);
+        tap1.write(&data[pos..]);
         let mut buffer = String::new();
         tap1.read_to_string(&mut buffer)?;
         println!(buffer);
